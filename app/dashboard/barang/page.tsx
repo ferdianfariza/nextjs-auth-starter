cbec5d9c525769ed33a0a2a7a751a7d3ac2c7f25
urpapi <111202315074@mhs.dinus.ac.id>
2025-06-10 15:27:43 +0700

    fix barang

--- app/dashboard/barang/page.tsx
+++ app/dashboard/barang/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import prisma from "@/lib/prisma";
 import { columns } from "./columns"
 import { DataTable } from "./data-table"
--- app/dashboard/stok/page.tsx
+++ app/dashboard/stok/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import prisma from "@/lib/prisma";
 import { columns, Stok } from "./columns"; 
 import { DataTable } from "./data-table";
